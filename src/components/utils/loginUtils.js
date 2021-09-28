93775aeaa281d9664a2a727b891ee42e9e39d42d
Nevo <nevobrami@gmail.com>
2021-09-28 23:17:22 +0300

    update

--- src/components/utils/loginUtils.js
+++ src/components/utils/loginUtils.js
@@ -1,10 +1,15 @@
 import axios from 'axios'; 
-
-
+const defaultOptions = {
+   
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+let instance = axios.create(defaultOptions);
 
 export async function  getById(id) 
 {
-   let resp = await axios.get("https://nevobrami.herokuapp.com/employee/"+id) 
+   let resp = await instance.get("https://nevobrami.herokuapp.com/employee/"+id) 
     console.log(resp.data)
     return resp.data
 }
