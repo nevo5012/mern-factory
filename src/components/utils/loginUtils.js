11bd346e0d31ced0f98bdfbedda5b14a7af539b3
Nevo <nevobrami@gmail.com>
2021-09-28 18:20:33 +0300

    bug fix

--- src/components/utils/loginUtils.js
+++ src/components/utils/loginUtils.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export async function  getById(id) 
 {
-   let resp = await axios.get("https://nevobrami.herokuapp.com//employee/"+id) 
+   let resp = await axios.get("https://nevobrami.herokuapp.com/employee/"+id) 
     console.log(resp.data)
     return resp.data
 }
