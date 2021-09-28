5117086e87cf39224fbfb864b99dc70970913e43
Nevo <nevobrami@gmail.com>
2021-09-28 17:42:24 +0300

    commit

--- src/components/utils/loginUtils.js
+++ src/components/utils/loginUtils.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export async function  getById(id) 
 {
-   let resp = await axios.get("http://localhost:8000/api/employee/"+id) 
+   let resp = await axios.get("https://dashboard.heroku.com/apps/nevobrami/employee/"+id) 
     console.log(resp.data)
     return resp.data
 }
