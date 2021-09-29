e9dc7d6ad35ec82f94784a63ecb7916d44bbaf1b
Nevo <nevobrami@gmail.com>
2021-09-29 11:15:53 +0300

    Done

--- src/components/utils/loginUtils.js
+++ src/components/utils/loginUtils.js
@@ -9,7 +9,7 @@ let instance = axios.create(defaultOptions);
 
 export async function  getById(id) 
 {
-   let resp = await instance.get("http://nevobrami.herokuapp.com/api/employee/"+id) 
+   let resp = await instance.get("https://nevobrami.herokuapp.com/api/employee/"+id) 
     console.log(resp.data)
     return resp.data
 }
